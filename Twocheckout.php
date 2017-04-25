156b7e83196ebc5fff0583b1a60eb490fde25c8e
GplCart <gplcart.software@gmail.com>
2017-04-25 18:31:01 +0300

    Refactoring main class

--- Twocheckout.php
+++ Twocheckout.php
@@ -9,16 +9,13 @@
 
 namespace gplcart\modules\twocheckout;
 
-use gplcart\core\Config;
-use gplcart\core\models\Order as OrderModel,
-    gplcart\core\models\Language as LanguageModel,
-    gplcart\core\models\Transaction as TransactionModel;
-use gplcart\modules\omnipay_library\OmnipayLibrary as OmnipayLibraryModule;
+use gplcart\core\Module;
+use gplcart\core\models\Language as LanguageModel;
 
 /**
  * Main class for 2 Checkout module
  */
-class Twocheckout
+class Twocheckout extends Module
 {
 
     /**
@@ -40,10 +37,10 @@ class Twocheckout
     protected $controller;
 
     /**
-     * 2 Checkout Omnipay instance
-     * @var \Omnipay\TwoCheckoutPlus\Gateway $gateway
+     * Language model instance
+     * @var \gplcart\core\models\Language $language
      */
-    protected $gateway;
+    protected $language;
 
     /**
      * Order model instance
@@ -52,83 +49,13 @@ class Twocheckout
     protected $order;
 
     /**
-     * Transaction model instance
-     * @var \gplcart\core\models\Transaction $transaction
-     */
-    protected $transaction;
-
-    /**
-     * Language model instance
-     * @var \gplcart\core\models\Language $language
-     */
-    protected $language;
-
-    /**
-     * Config class instance
-     * @var \gplcart\core\Config $config
-     */
-    protected $config;
-
-    /**
-     * Omnipay library module instance
-     * @var \gplcart\modules\omnipay_library\OmnipayLibrary
-     */
-    protected $omnipay_library_module;
-
-    /**
-     * Constructor
-     * @param Config $config
      * @param LanguageModel $language
-     * @param OrderModel $order
-     * @param TransactionModel $transaction
-     * @param OmnipayLibraryModule $omnipay_library_module
      */
-    public function __construct(Config $config, LanguageModel $language,
-            OrderModel $order, TransactionModel $transaction,
-            OmnipayLibraryModule $omnipay_library_module)
+    public function __construct(LanguageModel $language)
     {
-        $this->order = $order;
-        $this->config = $config;
+        parent::__construct();
+
         $this->language = $language;
-        $this->transaction = $transaction;
-
-        $this->omnipay_library_module = $omnipay_library_module;
-        $this->gateway = $this->omnipay_library_module->getGatewayInstance('TwoCheckoutPlus');
-    }
-
-    /**
-     * Module info
-     * @return array
-     */
-    public function info()
-    {
-        return array(
-            'core' => '1.x',
-            'name' => '2 Checkout',
-            'version' => '1.0.0-alfa.1',
-            'description' => 'Provides 2 Checkout payment method',
-            'author' => 'Iurii Makukh <gplcart.software@gmail.com>',
-            'license' => 'GNU General Public License 3.0',
-            'dependencies' => array('omnipay_library' => '>= 1.0'),
-            'configure' => 'admin/module/settings/twocheckout',
-            'settings' => $this->getDefaultSettings()
-        );
-    }
-
-    /**
-     * Returns an array of default module settings
-     * @return array
-     */
-    protected function getDefaultSettings()
-    {
-        return array(
-            'test' => true,
-            'status' => true,
-            'order_status_success' => $this->order->getStatusProcessing(),
-            // Gateway specific params
-            'accountNumber' => '',
-            'secretWord' => ''
-        );
     }
 
     /**
@@ -151,7 +78,11 @@ class Twocheckout
      */
     public function hookModuleEnableBefore(&$result)
     {
-        $this->validateGateway($result);
+        try {
+            $this->getGatewayInstance();
+        } catch (\InvalidArgumentException $ex) {
+            $result = $ex->getMessage();
+        }
     }
 
     /**
@@ -160,18 +91,28 @@ class Twocheckout
      */
     public function hookModuleInstallBefore(&$result)
     {
-        $this->validateGateway($result);
+        try {
+            $this->getGatewayInstance();
+        } catch (\InvalidArgumentException $ex) {
+            $result = $ex->getMessage();
+        }
     }
 
     /**
-     * Checks the gateway object is loaded
-     * @param mixed $result
+     * Get gateway instance
+     * @return object
+     * @throws \InvalidArgumentException
      */
-    protected function validateGateway(&$result)
+    protected function getGatewayInstance()
     {
-        if (!$this->gateway instanceof \Omnipay\TwoCheckoutPlus\Gateway) {
-            $result = $this->language->text('Unable to load @name gateway', array('@name' => '2 Checkout'));
+        /* @var $object \gplcart\modules\omnipay_library\OmnipayLibrary */
+        $object = $this->getInstance('gplcart\\modules\\omnipay_library\\OmnipayLibrary');
+
+        if (!$object instanceof \Omnipay\TwoCheckoutPlus\Gateway) {
+            throw new \InvalidArgumentException('Object is not instance of Omnipay\TwoCheckoutPlus\Gateway');
         }
+
+        return $object->getGatewayInstance('TwoCheckoutPlus');
     }
 
     /**
@@ -211,13 +152,14 @@ class Twocheckout
     /**
      * Implements hook "order.add.before"
      * @param array $order
+     * @param \gplcart\core\models\Order $object
      */
-    public function hookOrderAddBefore(array &$order)
+    public function hookOrderAddBefore(array &$order, $object)
     {
         // Adjust order status before creation
         // We want to get payment in advance, so assign "awaiting payment" status
         if ($order['payment'] === 'twocheckout') {
-            $order['status'] = $this->order->getStatusAwaitingPayment();
+            $order['status'] = $object->getStatusAwaitingPayment();
         }
     }
 
@@ -237,10 +179,11 @@ class Twocheckout
      * Implements hook "order.complete.page"
      * @param array $order
      * @param \gplcart\core\controllers\frontend\Controller $controller
-     * @return null
+     * @param \gplcart\core\models\Order $model
      */
-    public function hookOrderCompletePage(array $order, $controller)
+    public function hookOrderCompletePage(array $order, $controller, $model)
     {
+        $this->order = $model;
         $this->data_order = $order;
         $this->controller = $controller;
 
@@ -256,7 +199,8 @@ class Twocheckout
     protected function complete()
     {
         if ($this->controller->isQuery('paid')) {
-            $this->response = $this->gateway->completePurchase($this->getPurchaseParams())->send();
+            $gateway = $this->getGatewayInstance();
+            $this->response = $gateway->completePurchase($this->getPurchaseParams())->send();
             $this->processResponse();
         }
     }
@@ -268,19 +212,20 @@ class Twocheckout
     {
         if ($this->controller->isPosted('pay')) {
 
-            $this->gateway->setDemoMode((bool) $this->setting('test'));
-            $this->gateway->setCurrency($this->data_order['currency']);
-            $this->gateway->setSecretWord($this->setting('secretWord'));
-            $this->gateway->setAccountNumber($this->setting('accountNumber'));
+            $gateway = $this->getGatewayInstance();
+            $gateway->setDemoMode((bool) $this->setting('test'));
+            $gateway->setCurrency($this->data_order['currency']);
+            $gateway->setSecretWord($this->setting('secretWord'));
+            $gateway->setAccountNumber($this->setting('accountNumber'));
 
-            $this->gateway->setCart(array(array(
+            $gateway->setCart(array(array(
                     'quantity' => 1,
                     'type' => 'product',
                     'price' => $this->data_order['total_formatted_number'],
                     'name' => $this->language->text('Order #@num', array('@num' => $this->data_order['order_id']))
             )));
 
-            $this->response = $this->gateway->purchase($this->getPurchaseParams())->send();
+            $this->response = $gateway->purchase($this->getPurchaseParams())->send();
 
             if ($this->response->isRedirect()) {
                 $this->response->redirect();
@@ -347,11 +292,8 @@ class Twocheckout
      */
     protected function updateOrderStatus()
     {
-        $data = array(
-            'status' => $this->setting('order_status_success'));
+        $data = array('status' => $this->setting('order_status_success'));
         $this->order->update($this->data_order['order_id'], $data);
-
-        // Load fresh data
         $this->data_order = $this->order->get($this->data_order['order_id']);
     }
 
@@ -369,7 +311,9 @@ class Twocheckout
             'gateway_transaction_id' => $this->response->getTransactionReference()
         );
 
-        return $this->transaction->add($transaction);
+        /* @var $object \gplcart\core\models\Transaction */
+        $object = $this->getInstance('gplcart\\core\\models\\Transaction');
+        return $object->add($transaction);
     }
 
 }
